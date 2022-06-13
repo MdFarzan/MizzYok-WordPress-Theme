b2b79c5a19bfabd1ca06ee9a00cfda52d2c29859
MdFarzan <mdfarzan776@gmail.com>
2022-06-13 11:05:51 +0530

    blog sidebar added

--- functions.php
+++ functions.php
@@ -24,4 +24,25 @@ if(!function_exists('my_reg_menu')){
 }
 
 
+// adding sidebar widget
+if(!function_exists('my_reg_sidebars')){
+
+    function my_reg_sidebars(){
+
+        // blog sidebar
+        register_sidebar( array(
+            'name'          => __( 'Blog Sidebar', TEXT_DOMAIN ),
+            'id'            => 'blog-sidebar',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</aside>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        ) );
+
+    }
+
+    add_action( 'widgets_init', 'my_reg_sidebars' );
+
+}
+
 ?>
