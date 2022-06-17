ec91a4e3d7ecf8ff2beb8fb553f1952e4aef75b3
MdFarzan <mdfarzan776@gmail.com>
2022-06-17 12:47:31 +0530

    page-title.php added for seprate pages

---
+++ template-parts/page-title.php
@@ -0,0 +1,16 @@
+<?php 
+
+    /* 
+    page-title.php
+    contains inner page title template
+    */
+
+?>
+
+<!-- title content starts -->
+<div class="page-title">
+        <div class="container py-4 py-md-5">
+            <h1><?php wp_title('', true); ?></h1>
+        </div>
+</div>
+<!-- title content ends -->
