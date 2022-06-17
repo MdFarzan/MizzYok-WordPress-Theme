b142c5890f76b05494762adaae4a4f23ad5f992b
MdFarzan <mdfarzan776@gmail.com>
2022-06-17 12:54:40 +0530

    link not working on header logo fixed

--- header.php
+++ header.php
@@ -16,11 +16,11 @@
                         <a class="navbar-brand tir-color" id="header-logo" href="#">
                             <?php 
                                 if ( has_custom_logo() ) {
-                                    echo '<img id="header-logo" src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+                                    echo '<a href="'. get_home_url() .'"><img id="header-logo" src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '"></a>';
                                 }
                                 
                                 if((get_theme_mod('header_text') !== 0) && (get_bloginfo('description') !== '')) {
-                                    echo '<h1>'.get_bloginfo('name').'</h1>';
+                                    echo '<h1><a href="'. get_home_url() .'">'.get_bloginfo('name').'</a></h1>';
                                     echo '<p class="tagline">'.get_bloginfo('description').'</p>';
                                 }
                             
