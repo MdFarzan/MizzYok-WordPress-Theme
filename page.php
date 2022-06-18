54c58d01fa194de3b31aa9f7983eef0a7834ca5c
MdFarzan <mdfarzan776@gmail.com>
2022-06-18 10:15:38 +0530

    page.php for single page added

---
+++ page.php
@@ -0,0 +1,46 @@
+<?php 
+
+    /* 
+        page.php
+        page for single page taxonimies
+    */
+    the_post();
+    require_once('template-parts/head.php');
+?>
+
+<body class="prim-bg <?php body_class('prim-bg') ?>">
+    <?php  wp_body_open(); ?>
+    <!-- body starts -->
+    <div id="wrapper">
+        <div class="container">
+            <div class="hero">
+                <header>
+                    <?php
+                        get_header(); 
+                    ?>
+                </header>
+            </div>
+        </div>
+        <?php require_once('template-parts/page-title.php'); ?>
+
+        <div class="page-content-wrap">
+            <div class="container">
+                        
+
+                <div class="post-content">
+                    <?php
+                        echo the_content();
+                    ?>
+
+
+
+                </div>
+                   
+                
+            </div>
+        </div>
+
+    </div>
+</body>
+
+<?php get_footer(); ?>
